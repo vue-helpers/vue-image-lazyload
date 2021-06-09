b675dcb885e777a2dbb469f959494ceb63b749f0
romanbeniukh <roman.beniukh@gmail.com>
2021-06-09 09:51:27 +0300

    update build

---
+++ build/rollup.config.es.js
@@ -0,0 +1,11 @@
+import base from './rollup.config.base'
+
+const config = Object.assign({}, base, {
+  output: {
+    name: 'vue-image-lazyload',
+    file: 'dist/vue-image-lazyload.esm.js',
+    format: 'es',
+  },
+})
+
+export default config
